1031c37a94984a09fa71d4ebde61fb24c3f6635a
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-23 13:57:49 +0400

    Add find dublicates logic

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Скрипт предназначен для рекурсивного поиска дубликатов файлов. 
+Под понятие `дубликат` попадают файлы с одинаковым именем и одинаковым размером.
+
+При запуске скрипта параметром ему нужно передать путь к папке для сканирования `python3 duplicates.py DIR_PATH`;
+
+Примеры работы скрипта:
+
+```
+> python3 duplicates.py
+Please pass directory as a param
+
+> python3 duplicates.py 11234
+Please pass correct directory name
+
+> python3 duplicates.py 1
+No dublicates detected
+
+> python3 duplicates.py .
+Next dublicates detected:
+Group `test.1 with size 0`
+./1/test.1
+./3/test.1
+./2/test.1
+Group `HEAD with size 198`
+./.git/logs/HEAD
+./.git/logs/refs/remotes/origin/HEAD
+```
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,56 @@
+import sys
+import os
+import os.path
 
-if __name__ == '__main__':
-    pass
+
+def deep_files_iterate(start_path, processor=lambda _: None):
+    for entity in os.scandir(start_path):
+        if entity.is_dir():
+            deep_files_iterate(entity.path, processor)
+        else:
+            processor(entity.path)
+
+
+class DuplicatesStatHandler:
+    def __init__(self):
+        self.storage = {}
+
+    def get_ident_key(self, path):
+        filename = os.path.basename(path)
+        file_size = os.stat(path).st_size
+        return "{} with size {}".format(filename, file_size)
+
+    def add_file(self, path):
+        key = self.get_ident_key(path)
+        if key not in self.storage:
+            self.storage[key] = []
+
+        self.storage[key].append(path)
+
+    def get_dublicates(self):
+        keys_with_dublicates = filter(lambda x: len(self.storage[x]) > 1, self.storage.keys())
+        return map(lambda x: (x, self.storage[x]), keys_with_dublicates)
+
+
+if len(sys.argv) < 2:
+    sys.exit("Please pass directory as a param")
+
+dirname = sys.argv[1]
+
+if not os.path.isdir(dirname):
+    sys.exit("Please pass correct directory name")
+
+duplicate_handler = DuplicatesStatHandler()
+
+deep_files_iterate(sys.argv[1], duplicate_handler.add_file)
+
+duplicates = list(duplicate_handler.get_dublicates())
+
+if len(duplicates) == 0:
+    print("No dublicates detected")
+else:
+    print("Next dublicates detected:")
+
+    for group_name, duplicates_group in duplicates:
+        print("Group `{}`".format(group_name))
+        print("\n".join(duplicates_group))
